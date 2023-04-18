710f18a411661962b88be118238afba4432df342
Hoongithub2022 <gns2506@gmail.com>
2023-04-18 13:48:54 +0900

    2023-04-18

--- User/src/components/user1/User1Main.vue
+++ User/src/components/user1/User1Main.vue
@@ -1,7 +1,9 @@
 <template>
     <h4>User1 Main</h4>
+
     <router-link to="/user1/register">등록</router-link>&nbsp;/
     <router-link to="/user1/list">목록</router-link>&nbsp;/
+
     <router-view></router-view>
 </template>
 
